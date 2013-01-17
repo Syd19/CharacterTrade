b50b89fe1160e84b26e9dc20adeb678ec9785a48
Syd19 <sydns12@hotmail.com>
2013-01-17 12:09:39 +0000

    Useless info removed

--- submited_chars.sql
+++ submited_chars.sql
@@ -1,19 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : Project57
-Source Server Version : 50528
-Source Host           : 78.46.97.186:3306
-Source Database       : characters
-
-Target Server Type    : MYSQL
-Target Server Version : 50528
-File Encoding         : 65001
-
-Date: 2013-01-12 12:09:57
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for `submited_chars`
@@ -30,10 +14,3 @@ CREATE TABLE `submited_chars` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of submited_chars
--- ----------------------------
-INSERT INTO `submited_chars` VALUES ('49', 'Limao', '80', '', 'Pala', '', '0');
-INSERT INTO `submited_chars` VALUES ('50', 'Welly', '80', '', 'Warrior', '', '0');
-INSERT INTO `submited_chars` VALUES ('51', 'Limao', '80', '', 'Pala', '', '0');
-INSERT INTO `submited_chars` VALUES ('52', 'Nivia', '80', '', 'Druid', 'd', '15');
